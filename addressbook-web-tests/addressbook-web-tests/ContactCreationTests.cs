9fa974c64ce6d3ef52b7d5779a973fe5075c530e
Artur <baker306@bk.ru>
2021-02-16 12:21:12 +0300

    Создал класс для тестовых данных для контактов.

--- addressbook-web-tests/addressbook-web-tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/ContactCreationTests.cs
@@ -45,7 +45,10 @@ namespace addressbook_web_tests
             GoToHomePage();
             Login(new AccountData("admin", "secret"));
             GoToNewContactPage();
-            FillContactForm("aaa", "bbb");
+            ContactData contact = new ContactData();
+            contact.Firstname = "Ivan";
+            contact.Lastname = "Ivanov";
+            FillContactForm(contact);
             SubmitContactCreation();
             BackToHomePage();
             Logout();
@@ -66,14 +69,14 @@ namespace addressbook_web_tests
             driver.FindElement(By.Name("submit")).Click();
         }
 
-        private void FillContactForm(string firstname, string lastname)
+        private void FillContactForm(ContactData contact)
         {
             driver.FindElement(By.Name("firstname")).Click();
             driver.FindElement(By.Name("firstname")).Clear();
-            driver.FindElement(By.Name("firstname")).SendKeys(firstname);
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
             driver.FindElement(By.Name("lastname")).Click();
             driver.FindElement(By.Name("lastname")).Clear();
-            driver.FindElement(By.Name("lastname")).SendKeys(lastname);
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
         }
 
         private void GoToNewContactPage()
---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class ContactData
+    {
+        private string firstname = "";
+        private string lastname = "";
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+}
